b43f345e68ebf10a2ef8255907494c9aa05152e9
Samuel Jonathan <Omohsam81@gmail.com>
2021-10-10 13:13:53 +0100

    week 04 assignment

---
+++ src/sam/Hello.java
@@ -0,0 +1,12 @@
+package sam;
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args){
+        Scanner name = new Scanner(System.in);
+        System.out.printf("Enter your name: ");
+        String userName = name.nextLine();
+        System.out.println("Hello, my name is "+ userName);
+    }
+}
